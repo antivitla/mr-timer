cc306df209fbd1d640ad53062ee9d0468d0d148b
Виталий Кулешин <antivitla@gmail.com>
2017-06-25 22:22:00 +0300

    remove all

--- src/utils/debounce.js
+++
@@ -1,7 +0,0 @@
-export default function debounce () {
-  let debounceTimeout
-  return function (f, delay) {
-    clearTimeout(debounceTimeout)
-    debounceTimeout = setTimeout(f, delay)
-  }
-}
--- src/utils/focus-and-select.js
+++
@@ -1,6 +0,0 @@
-export default function focusAndSelect (element, start, stop) {
-  setTimeout(() => {
-    element.focus()
-    element.setSelectionRange(start, stop)
-  }, 50)
-}
