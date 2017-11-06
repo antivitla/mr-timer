a7995d0f0046fad027675597a5f2cee0d386ac0c
Виталий Кулешин <antivitla@gmail.com>
2017-11-06 18:25:21 +0300

    Deploy script change

--- bin/deploy.cmd
+++ bin/deploy.cmd
@@ -1,3 +1,4 @@
+npm run build
 ssh u49801@u49801.netangels.ru "rm -rf mr-woodman.ru/www/titamota/static/js && rm -rf mr-woodman.ru/www/titamota/static/css"
 scp -r dist/static/js/ u49801@u49801.netangels.ru:~/mr-woodman.ru/www/titamota/static/js/
 scp -r dist/static/css/ u49801@u49801.netangels.ru:~/mr-woodman.ru/www/titamota/static/css/
